5246711c87b5cefa1c1c9077fd2ffa693afd717f
王鹏鹏 <874934733@qq.com>
2024-05-07 16:05:56 +0800

    1.添加我的页面UI

--- main/src/main/java/com/yingyang/main/fragment/MainFragment.kt
+++ main/src/main/java/com/yingyang/main/fragment/MainFragment.kt
@@ -3,6 +3,7 @@ package com.yingyang.main.fragment
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.yingyang.main.databinding.FragmentMainBinding
 import com.yingyangfly.baselib.base.BaseFragment
+import com.yingyangfly.baselib.ext.initCenterTitle
 import com.yingyangfly.baselib.router.RouterUrlCommon
 
 /**
@@ -12,6 +13,8 @@ import com.yingyangfly.baselib.router.RouterUrlCommon
 class MainFragment : BaseFragment<FragmentMainBinding>() {
 
     override fun initViews() {
+        initCenterTitle("我的")
+
 
     }
 
