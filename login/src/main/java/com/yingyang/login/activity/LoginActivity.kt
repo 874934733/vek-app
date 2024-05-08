e2dbb95cdabd84c5213f0567a826faf420e4ae07
王鹏鹏 <874934733@qq.com>
2024-05-08 09:55:50 +0800

    1.添加登录模块

---
+++ login/src/main/java/com/yingyang/login/activity/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.yingyang.login.activity
+
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.yingyang.login.databinding.ActivityLoginBinding
+import com.yingyangfly.baselib.base.BaseActivity
+import com.yingyangfly.baselib.router.RouterUrlCommon
+
+/**
+ * 登录activity
+ */
+@Route(path = RouterUrlCommon.login)
+class LoginActivity : BaseActivity<ActivityLoginBinding>() {
+    override fun initViews() {
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initData() {
+
+    }
+}
